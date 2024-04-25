7127df6e027ca1a064bfe124ee62163e7dd67a0c
Anuj Sharma <cloudu07@gmail.com>
2024-04-25 15:26:06 +0530

    change data

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: import.meta.env.VITE_TMDB_API,
+    api_key: "7863fb9d4d21f8ba7cd67ac8ab423546",
   },
 });
